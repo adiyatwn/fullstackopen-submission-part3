09fd66f91be02f05fb6e988d3b68650c0802ba55
Adiyat Wira Negara <kazutonine@gmail.com>
2024-10-10 20:27:46 +0700

    Add validation on backend

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ app.post('/api/persons', (request, response, next) => {
         person.save().then(savedPerson => {
           response.json(savedPerson)
         })
+          .catch(error => next(error))
       }
     })
 })
@@ -87,7 +88,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       if (updatedPerson) {
         response.json(updatedPerson)
@@ -105,6 +106,12 @@ app.get('/info', (request, response) => {
   })
 })
 
+const unknownEndpoint = (request, response) => {
+  return response.status(404).json({ error: "Unknown endpoint" })
+}
+
+app.use(unknownEndpoint)
+
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
@@ -112,6 +119,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({
       error: "malformatted id"
     })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
