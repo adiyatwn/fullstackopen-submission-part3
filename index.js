4f8bf981e0741e3d8ff43ead52503666cfccd19e
Adiyat Wira Negara <kazutonine@gmail.com>
2024-10-10 13:20:01 +0700

    Fix PORT

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get('/info', (request, response) => {
   return response.send(`<p>Phonebook has info for ${totalPersons} people</p><p>${date}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
